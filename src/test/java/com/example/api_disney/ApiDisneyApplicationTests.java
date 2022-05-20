c82ceeb04e3784df8ee3047ad003c9bb054e47ce
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-05-20 17:55:16 -0500

    se crea proyecto en spring boot

---
+++ src/test/java/com/example/api_disney/ApiDisneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_disney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDisneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
