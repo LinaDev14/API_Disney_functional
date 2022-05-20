81c7907cd20a56e16b546a93e55f36db4ada34bf
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-05-20 18:33:48 -0500

    se crea clase Category

--- src/main/java/com/example/api_disney/domain/collections/Category.java
+++ src/main/java/com/example/api_disney/domain/collections/Category.java
@@ -1,4 +1,25 @@
 package com.example.api_disney.domain.collections;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+
+@Data
+@Document
 public class Category {
+
+    @Id
+    private String characterId;
+    private String characterName;
+    private Integer characterAge;
+    private Float characterWeight;
+    private String characterStory;
+    private List<String> characterMediaIds;
+    
 }
---
+++ src/main/java/com/example/api_disney/domain/dtos/CategoryDTO.java
@@ -0,0 +1,4 @@
+package com.example.api_disney.domain.dtos;
+
+public class CategoryDTO {
+}
