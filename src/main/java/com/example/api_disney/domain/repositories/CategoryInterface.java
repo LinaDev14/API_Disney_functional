0769c92d4da7adbaa4dc086eba1000cae163e43e
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-05-20 21:15:50 -0500

    se crea categoryInterface

--- src/main/java/com/example/api_disney/domain/repositories/CategoryInterface.java
+++ src/main/java/com/example/api_disney/domain/repositories/CategoryInterface.java
@@ -1,4 +1,7 @@
 package com.example.api_disney.domain.repositories;
 
-public interface CategoryInterface {
+import com.example.api_disney.domain.collections.Category;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CategoryInterface extends MongoRepository<Category, String> {
 }
--- src/main/java/com/example/api_disney/domain/repositories/CharacterImageInterface.java
+++ src/main/java/com/example/api_disney/domain/repositories/CharacterImageInterface.java
@@ -1,4 +1,7 @@
 package com.example.api_disney.domain.repositories;
 
-public interface CharacterImageInterface {
+import com.example.api_disney.domain.collections.CharacterImage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CharacterImageInterface extends MongoRepository<CharacterImage, String> {
 }
