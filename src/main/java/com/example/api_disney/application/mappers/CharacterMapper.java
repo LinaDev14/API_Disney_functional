7c6e173e2429d394ced81861f10bb1920e292bc3
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-05-20 23:41:21 -0500

    se crean mappers

---
+++ src/main/java/com/example/api_disney/application/mappers/CharacterMapper.java
@@ -0,0 +1,4 @@
+package com.example.api_disney.application.mappers;
+
+public class CharacterMapper {
+}
---
+++ src/main/java/com/example/api_disney/application/mappers/MediaMapper.java
@@ -0,0 +1,7 @@
+package com.example.api_disney.application.mappers;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MediaMapper {
+}
