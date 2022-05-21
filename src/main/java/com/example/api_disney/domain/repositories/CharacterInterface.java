49dbedfe6d1baa010a4db545406e9314d068af74
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-05-20 21:16:50 -0500

    se crea characterInterface

--- src/main/java/com/example/api_disney/domain/repositories/CharacterInterface.java
+++ src/main/java/com/example/api_disney/domain/repositories/CharacterInterface.java
@@ -1,4 +1,7 @@
 package com.example.api_disney.domain.repositories;
 
-public interface CharacterInterface {
+import com.example.api_disney.domain.collections.Character;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CharacterInterface extends MongoRepository<Character, String> {
 }
