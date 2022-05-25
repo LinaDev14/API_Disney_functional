51de63e32c97399ba03d832902291705e200792f
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-05-24 21:29:36 -0500

    se crea mapToCategoryDto

---
+++ src/main/java/com/example/api_disney/application/mappers/CategoryMapper.java
@@ -0,0 +1,18 @@
+package com.example.api_disney.application.mappers;
+
+import com.example.api_disney.domain.collections.Category;
+import com.example.api_disney.domain.dtos.CategoryDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class CategoryMapper {
+
+    public Function<Category, CategoryDTO> mapToCategoryDto(){
+        return category -> new CategoryDTO(
+                category.getCategoryId(),
+                category.getCategoryName()
+        );
+    }
+}
