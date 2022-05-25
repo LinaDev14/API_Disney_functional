1401543c0309f40750dcc5c52d1b363b0f5573e8
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-05-24 20:55:27 -0500

    se crea mapToCharacterInDto

--- src/main/java/com/example/api_disney/application/mappers/CharacterMapper.java
+++ src/main/java/com/example/api_disney/application/mappers/CharacterMapper.java
@@ -1,9 +1,21 @@
 package com.example.api_disney.application.mappers;
 
+import com.example.api_disney.domain.collections.Character;
+import com.example.api_disney.domain.dtos.CharacterInDTO;
 import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
 
 @Component
 public class CharacterMapper {
 
-    
+    public Function<Character, CharacterInDTO> mapToCharacterInDto(){
+        return character -> new CharacterInDTO(
+                character.getCharacterId(),
+                character.getCharacterName(),
+                character.getCharacterAge(),
+                character.getCharacterWeight(),
+                character.getCharacterStory()
+        );
+    }
 }
