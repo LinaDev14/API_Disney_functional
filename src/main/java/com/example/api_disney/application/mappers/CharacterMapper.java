be270c55b8e125cd53e378dc5a6a30f1a2f13813
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-05-24 21:15:15 -0500

    se crea mapToCharacterOutDto

--- src/main/java/com/example/api_disney/application/mappers/CharacterMapper.java
+++ src/main/java/com/example/api_disney/application/mappers/CharacterMapper.java
@@ -2,8 +2,12 @@ package com.example.api_disney.application.mappers;
 
 import com.example.api_disney.domain.collections.Character;
 import com.example.api_disney.domain.dtos.CharacterInDTO;
+import com.example.api_disney.domain.dtos.CharacterOutDTO;
+import com.example.api_disney.domain.dtos.MediaDTO;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 @Component
@@ -28,6 +32,17 @@ public class CharacterMapper {
                 characterInDTO.getCharacterWeight(),
                 characterInDTO.getCharacterStory()
 
+        );
+    }
+
+    public BiFunction<Character, List<MediaDTO>, CharacterOutDTO> mapToCharacterOutDto(){
+        return (character, mediaDTOS) -> new CharacterOutDTO(
+                character.getCharacterId(),
+                character.getCharacterName(),
+                character.getCharacterAge(),
+                character.getCharacterWeight(),
+                character.getCharacterStory(),
+                mediaDTOS
         );
     }
 }
